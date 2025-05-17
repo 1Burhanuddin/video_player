2ef92b9c1ad5ddd141489906f3760471995cbebd
1burhanuddin <111burhanuddin@gmail.com>
2025-05-17 17:27:01 +0530

    Added progress bar and removed ads 4

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
   build: {
     outDir: 'dist',
     sourcemap: false,
@@ -24,5 +15,15 @@ export default defineConfig({
         drop_debugger: true
       }
     }
-  }
+  },
+  // Remove the proxy configuration for production
+  server: process.env.NODE_ENV === 'development' ? {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  } : undefined
 })
